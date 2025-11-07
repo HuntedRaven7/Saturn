77886db1c0a15010b56497556f06f515cf7bc72b
HuntedRaven7 <EdwardGroves2007@proton.me>
2025-11-07 00:29:45 -0500

    copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,7 @@ dnf5 install -y tmux neovim ghostty
 dnf5 -y copr enable scottames/ghostty
 dnf5 -y install ghostty
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable scottames/ghostty
 
 #### Example for enabling a System Unit File
 
